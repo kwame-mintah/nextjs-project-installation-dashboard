d5099e58374c5869c13d88f9399a81fa7429e889
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-05-29 17:04:19 +0100

    feat(request): implement making request to backend service
    button added to home screen to test making a request, `BACKEND_SERVICE_URL` needs to be set as an environment variable. minor changes to remove references of create-react-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+const baseURL = `${process.env.BACKEND_SERVICE_URL}`;
+
+const getUser = () => {
+  fetch(baseURL + '/user/').then((response) => {
+    return response.json;
+  });
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getUser}>GET /user/</button>
       </header>
     </div>
   );
