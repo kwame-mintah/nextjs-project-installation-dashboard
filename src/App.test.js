92dbdf591f5931f0b2a564345ac180adec382c14
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-06-20 18:12:55 +0100

    refactor(react): delete react auto-generated files
    these files are no longer needed, will be moving to [nextjs](https://nextjs.org/).
    
    Refs #TASK-15

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders get user button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/GET user/i);
-  expect(linkElement).toBeInTheDocument();
-});
