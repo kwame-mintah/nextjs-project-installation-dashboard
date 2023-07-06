0b351c6a8747c7f56515dba45e61c7912cf09f9a
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-07-07 00:39:09 +0100

    feat(layout): create layout components for web app
    new components created for layout, added `appbar` and removed styling from index page.
    
    Refs #TASK-22

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import * as React from 'react';
-import { CssBaseline } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { Copyright } from './components/copyright';
+import { createTheme } from '@mui/material/styles';
+import NavBar from './components/appbar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+
+const defaultTheme = createTheme();
 
 export default function RootLayout({
   children,
@@ -11,15 +16,43 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <CssBaseline enableColorScheme={true} />
       <html lang="en">
         <head>
           <link rel="shortcut icon" href="./favicon.ico" />
-          <title>Automated Dashboard</title>
+          <title>Automation Dashboard</title>
         </head>
         <body>
-          <main>{children}</main>
-          <Copyright />
+          <ThemeProvider theme={defaultTheme}>
+            {/* Theme start */}
+            <Box sx={{ display: 'flex' }}>
+              {/* Box 1 start */}
+              <CssBaseline enableColorScheme={true} />
+              {/* Navbar */}
+              <NavBar />
+              {/* Box 2 start */}
+              <Box
+                component="main"
+                sx={{
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                      ? theme.palette.grey[100]
+                      : theme.palette.grey[900],
+                  flexGrow: 1,
+                  height: '100vh',
+                  overflow: 'auto',
+                }}
+              >
+                <Toolbar />
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                  <main>{children}</main>
+                  <Copyright />
+                </Container>
+                {/* Box 2 end */}
+              </Box>
+              {/* Box 1 end */}
+            </Box>
+            {/* Theme end */}
+          </ThemeProvider>
         </body>
       </html>
     </>
