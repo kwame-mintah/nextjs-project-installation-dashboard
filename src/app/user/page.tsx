e63dc31e63c196bf503670739656c35d15d6d0e4
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-07-08 15:18:49 +0100

    feat(user): display user details
    display all user details such as, first name, last name, role and location.
    
    Refs #TASK-28

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,30 +1,70 @@
 'use client';
 
-import Link from 'next/link';
+import { Grid, Paper, TextField, Typography } from '@mui/material';
+import { Container } from '@mui/system';
 
-const getBackendStatus = async () => {
-  return fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVICE_URL}/`)
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      }
-      return {
-        content: response.statusText,
-      };
+const userDetails = async () => {
+  return fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_SERVICE_URL}` + `/user/0x00/details`,
+  )
+    .then((response) => response.json())
+    .then((jsonData) => {
+      return jsonData;
     })
-    .then((response) => response.content)
     .catch((err) => console.log(err));
 };
 
 export default async function Page() {
+  const user = await userDetails();
+
   return (
     <>
-      <p>
-        This my user page <Link href="/"> Go back to home. </Link>{' '}
-        <button type="button" onClick={getBackendStatus}>
-          Lets see something
-        </button>
-      </p>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Typography variant="overline">User details</Typography>
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="First name"
+                defaultValue={user.firstName}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="Last name"
+                defaultValue={user.lastName}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="Role"
+                defaultValue={user.role}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="Location"
+                defaultValue={user.location}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <hr />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
