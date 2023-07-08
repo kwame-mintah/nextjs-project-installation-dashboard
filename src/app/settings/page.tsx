89b36a685973abce3263d644460fa43dad02db81
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-07-08 15:41:20 +0100

    feat(settings): display settings details
    display settings such as operating system and version.
    
    Refs #TASK-30

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,49 @@
-import Link from 'next/link';
+'use client';
 
-export default function Page() {
+import { Grid, Paper, Typography, TextField } from '@mui/material';
+import { Container } from '@mui/system';
+
+const userSettings = async () => {
+  return fetch(`${process.env.NEXT_PUBLIC_BACKEND_SERVICE_URL}` + `/settings/`)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      return jsonData;
+    })
+    .catch((err) => console.log(err));
+};
+
+export default async function Page() {
+  const setting = await userSettings();
+
   return (
-    <p>
-      This my settings page <Link href="/"> Go back to home. </Link>{' '}
-    </p>
+    <>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <Typography variant="overline">Settings</Typography>
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="Operating system (OS)"
+                defaultValue={setting.operatingSystem}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <hr />
+              <TextField
+                id="outlined-disabled"
+                label="OS version"
+                defaultValue={setting.version}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
